ce41124ceff1ae10582cc1b906aa2a94f36b8480
anthony.drogrez <anthony.drogrez@yahoo.com>
2021-05-07 12:22:22 +0200

    add script

---
+++ bandcamp.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import shutil
+import zipfile
+
+path = os.getcwd()
+#file = sys.argv[1:]
+for file in sys.argv:
+    if "bandcamp.py" not in file:
+        print ("The current working directory is %s" % path)
+        print("The dir is %s" % os.path.splitext(file)[0])
+        dir = os.path.splitext(file)[0]
+        try:
+            os.mkdir(dir)
+        except OSError:
+            print ("Creation of the directory %s failed" % dir)
+        else:
+            print ("Successfully created the directory %s " % dir)
+            shutil.move(file, dir + "\\" + file)
+            with zipfile.ZipFile(dir + "\\" + file, 'r') as zip_ref:
+                zip_ref.extractall(dir)
+            os.remove(dir + "\\" + file)
